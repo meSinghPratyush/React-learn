1914ce18b18d913e214698f5d5ca7dc32e5ccedc
meSinghPratyush <pratyush1555@gmail.com>
2025-04-21 16:28:54 +0530

    more changes

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
 function App() {
-  return <h1>React playground</h1>;
+  return (
+    <h1>
+      React playground | Today i learned how to create a react app and also how
+      to make changes in that app. - By Pratyush kumar.
+    </h1>
+  );
 }
 
 export default App;
