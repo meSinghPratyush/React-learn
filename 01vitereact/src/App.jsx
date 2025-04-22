fdf4eeb9b9c601e8281a0f5a6e7b4e7beb2e1a46
meSinghPratyush <pratyush1555@gmail.com>
2025-04-22 15:21:11 +0530

    Creating own element in react.

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,9 @@
 function App() {
+  const name = "Ayush";
   return (
     <h1>
       React playground | Today i learned how to create a react app and also how
-      to make changes in that app. - By Pratyush kumar.
+      to make changes in that app. - By {name}.
     </h1>
   );
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,14 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 import Test from "./test.jsx";
 
-createRoot(document.getElementById("root")).render(
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "https://www.google.com/" },
+  "Visit google"
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <>
       <App />
-      <Test />
+      <test />
+      {reactElement}
     </>
   </StrictMode>
 );
